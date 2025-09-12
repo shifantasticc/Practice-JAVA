366e3249669303cb84d74d43f2d76a2fcf807fdb
shifantasticc <shifastudyit@gmail.com>
2025-09-12 16:58:01 +0530

    Updating Java File

---
+++ Java-Basics/LeapYear.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Print whether the given yearc is a leap year or not.
+        System.out.println("Enter the year : ");
+        int year = sc.nextInt();
+
+        boolean x = (year % 4 ) == 0;
+        boolean y = (year % 100 ) != 0;
+        boolean z = ((year % 100 == 0 ) && (year % 400 == 0 ));
+
+        if (x && (y || z)) {
+            System.out.println(year + " " + "is a leap year");
+        } else {
+            System.out.println(year + " " + "is a not leap year");
+        }
+    }
+}
