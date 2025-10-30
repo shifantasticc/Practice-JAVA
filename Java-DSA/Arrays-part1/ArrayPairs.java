0d912f2514c24a9db69e3e7b2c756cb882b919cc
shifantasticc <shifastudyit@gmail.com>
2025-10-30 20:44:30 +0530

    Adding Array pairs program File

---
+++ Java-DSA/Arrays-part1/ArrayPairs.java
@@ -0,0 +1,20 @@
+public class ArrayPairs {
+
+    public static void getPairs(int numbers[]) {
+        int tp =0;
+        for (int i=0; i<numbers.length; i++) {
+            int curr = numbers[i];
+            for (int j=i+1; j<numbers.length; j++) {
+                System.out.print("(" + curr + "," + numbers[j] +  ") ");
+                tp++;
+            }
+            System.out.println(" ");
+        }
+        System.out.println("Total pairs are : " + tp);
+    }
+    
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        getPairs(numbers);
+    }
+}
