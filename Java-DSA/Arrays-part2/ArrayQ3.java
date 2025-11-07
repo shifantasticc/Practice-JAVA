534b06102c1da64c6227813fdad2a6895c414bd4
shifantasticc <shifastudyit@gmail.com>
2025-11-07 23:29:26 +0530

    Adding Ques 3 program File

---
+++ Java-DSA/Arrays-part2/ArrayQ3.java
@@ -0,0 +1,22 @@
+public class ArrayQ3 {
+    // Q.3
+    public static int maxProfit(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i=0; i<prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices[] = {7, 6, 4,  3, 1};
+        System.out.println("Maximum profit is : " + maxProfit(prices));
+    }
+}
