f0e0f3d2adeb4687431881a8a09a2f73cc3a7ad6
shifantasticc <shifastudyit@gmail.com>
2025-11-10 21:30:10 +0530

    Adding Brute Force program file

---
+++ Java-DSA/Arrays-part2/MAX-subarraySum/BruteForce.java
@@ -0,0 +1,30 @@
+public class BruteForce {
+    
+    // MAX subarray sum -1 Brute Force
+    public static void maxSubarraySum(int numbers[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i=0; i<numbers.length; i++) {
+            int start = i;
+            
+            for (int j=i; j<numbers.length; j++) {
+                int end = j;
+                currSum = 0;
+                for (int k=start; k<=end; k++) { // print
+                    currSum += numbers[k];
+                }
+                System.out.println(currSum);
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println("Max Sum is : " + maxSum);
+    }
+    
+    public static void main(String[] args) {
+        int numbers[] = {1, -2, 6, -1, 3};
+        maxSubarraySum(numbers);
+    }
+}
