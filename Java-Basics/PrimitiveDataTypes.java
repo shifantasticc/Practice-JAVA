64678d0bdf353cfa50440db8eed07b427a01b9d1
shifantasticc <shifastudyit@gmail.com>
2025-11-26 23:10:57 +0530

    Adding Primitive Data Types Program file

---
+++ Java-Basics/PrimitiveDataTypes.java
@@ -0,0 +1,31 @@
+//Primitive data types
+public class PrimitiveDataTypes {
+    public static void main(String args[]) {
+        //1 byte [-128 to 127] 256 nums
+        byte b = 8;
+        System.out.println(b);
+
+        //2 byte ['a'-'z', 'A'-'Z , special char]
+        char ch = 'a';
+        System.out.println(ch);
+
+        //1 byte [true, false]
+        boolean var = true;
+        System.out.println(var);
+
+        //4 byte [_._]
+        float price = 15.7f;
+        System.out.println(price);
+
+        //4 byte [-2B to +2B]
+        int num = 25;
+        System.out.println(num);
+
+        //2 byte
+        short n = 240;
+        System.out.println(n);
+
+        //long //8 byte
+        //double //8 byte [_._]
+    }
+}
